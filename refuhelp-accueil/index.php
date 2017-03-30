804b68d8f44ad5559ca24653ab9b9296a73364d2
amare16 <amarenighatu16@gmail.com>
2017-03-30 23:52:14 +0200

    added refuhelp-accueil folder for first page

---
+++ refuhelp-accueil/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>RefuHelp-Accueil Page</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<!-- navigation bar -->
+
+			<div class="navigation-bar">
+				<nav class="navigation-bar">
+					<a href=""><img class="logo" src="images/Refuhelp-Logo.png" alt="Refuhelp-Logo" width="194" height="67"></a>
+					<ul>
+						<li><a href="">Sign In</a></li>
+						<li><a href="">Sign Up</a></li>
+						<li><a href=""><img src="">fb pic</a></li>
+						<li><a href=""><img src="">twitter pic</a></li>
+						<li><a href=""><img src="">linkedin pic</a></li>
+					</ul>
+				</nav>
+			</div>
+
+			<!-- Welcome title -->
+			<div>
+				<h1>Bienvenue Sur RefuHelp</h1>
+			</div>
+
+			<!-- Search box -->
+			<div class="container">
+				<div class="row" id="search-am">
+					<div id="cutom-search-input">
+						<div class="input-group col-md-12">
+							<input type="text" class="search-query form-control" placeholder="Search">
+							<span class="input-group-btn">
+								<button class="btn btn-danger" type="button">
+									<span class="glyphicon glyphicon-search"></span>
+								</button>
+							</span>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</body>
+</html>
